6da7149ad0feaae468a55afa836507603b45b406
AlexVYard <alexvegayard@gmail.com>
2023-04-29 10:04:49 -0500

    error messages

--- src/components/login.js
+++ src/components/login.js
@@ -1,58 +1,109 @@
 // import logo from '../logo.svg';
 // import { onNavigate } from './lib/onNavigate.js';
-import /* React,  */{ useState, useEffect } from 'react'
+import React, { useState/* , useEffect */ } from 'react'
 import { /* Navigate,  */useNavigate } from "react-router-dom"
 import './styles.css';
+// import { getElementError } from '@testing-library/react'
+// import ReactDOM from "react-dom"
 
 function Login() {
+  /* const Results = () => (
+    <div>
+      <p
+        id="textoCorreoInvalido"
+        className="textoCorreoInvalido"
+        style={{visibility: this.state.showButton ? 'visible' : 'hidden' }}
+      >Escribe un correo valido</p>
+    </div>
+  ) */
   // onNavigate('/');
-  const [email, setEmail] = useState('')
+  // const showError = React.useState(false)
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState('')  
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
+  const [errorText, setErrorText] = useState("Error");
+  
   /* useEffect(() => {
     if (localStorage.getItem('user-info')) {
       navigate('/menu')
     }
   }, []) */
+  
+
   async function login() {
-    let item = {email, password}
-    console.log(item)
+
+      
+
+
+    let item = { email, password }
+    // console.log(item)
     let result = await fetch('http://localhost:8080/login', {
-      method: 'POST', 
+      method: 'POST',
       headers: {
         "content-type": "application/json"/* ,
         "accept": "application/json" */
       },
       body: JSON.stringify(item)
     })
+    // console.log(result)
     result = await result.json()
     // console.log("typeof result:", typeof result)
-    // console.log("result:", result)
+    console.log("result:", result)
     // console.log("JSON.stringify(item):", JSON.stringify(item))
     localStorage.setItem("user-info", JSON.stringify(result))
     // navigate("/menu")
     // console.log("typeof localStorage.getItem('user-info'): ", typeof localStorage.getItem('user-info'))
     // console.log("localStorage.getItem('user-info'):", localStorage.getItem('user-info'))
+
     if (typeof result === 'object') {
       navigate('/menu')
+    } else {
+      setErrorText(result)
+      // this.setState({ text: 'result' });
+      /* if (result === 'Email and password are required') {
+        document.getElementById('textoCorreoInvalido').innerText(result)
+      }
+      if (result === 'Email format is invalid') {
+        console.log("result:", result)
+      }
+      if (result === 'Password is too short') {
+        console.log("result:", result)
+      }
+      if (result === 'Cannot find user') {
+        console.log("result:", result)
+      }
+      if (result === 'Incorrect password') {
+        console.log("result:", result)
+      } */
     }
+
+    // ReactDOM.render(<Login />, document.querySelector("#container"))
   }
   return (
     <main className="PantallaInicio">
       <section className="cajaInicio">
         <img src={require('./img/img_libro_rojo.png')} alt="Imagen de libro"></img>
+
         <input
           type="text"
           placeholder=" E-mail"
           onChange={(e) => setEmail(e.target.value)}
         ></input>
-        <p id="textoCorreoInvalido" className="textoCorreoInvalido">Escribe un correo valido</p>
+
+        {error && <p
+          id="textoCorreoInvalido"
+          className="textoCorreoInvalido"
+          style={{ visibility: error ? 'visible' : 'hidden' }}
+        >{errorText}</p>}
+
         <input
           type="password"
           placeholder=" Password"
           onChange={(e) => setPassword(e.target.value)}
         ></input>
-        <button onClick={login} className="botonInicio">Log-in</button>
+
+        <button onClick={() => { setError(true); setErrorText(); login() }} className="botonInicio">Log-in</button>
       </section>
     </main>
   )
